043f8a101fafb1a6e49efef7f16c9e7ad8aa9990
Dragonite-0149 <07ruru@gmail.com>
2024-06-09 17:16:24 +0900

    ヘッダーを追加

--- reservation-practice/src/app/app.module.ts
+++ reservation-practice/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+};
